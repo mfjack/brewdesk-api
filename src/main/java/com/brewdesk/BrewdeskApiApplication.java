3d322a471094bd00c0cd345ecfb9b90935d3ce79
Jack <ferreira.marlon@live.com>
2026-06-25 17:39:25 -0300

    chore: initial project setup

---
+++ src/main/java/com/brewdesk/BrewdeskApiApplication.java
@@ -0,0 +1,13 @@
+package com.brewdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewdeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewdeskApiApplication.class, args);
+	}
+
+}
